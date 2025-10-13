c580ba19d670b8868b81fe321bad1bc4fe6200bb
unknown <akosiricardocosta@gmail.com>
2025-10-13 15:32:52 +0800

    Update web build

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "614747682"
+    serviceWorkerVersion: "2642746680"
   }
 });
